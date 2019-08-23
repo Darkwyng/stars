45d8c8c2190bc8b8f21f198012b540b2b3c0a98d
Pim <>
2019-08-23 19:16:53 +0200

    use deep stubs to mock builders

--- parent/addons/src/test/java/com/pim/stars/planets/PlanetTestConfiguration.java
+++ parent/addons/src/test/java/com/pim/stars/planets/PlanetTestConfiguration.java
@@ -1,18 +1,17 @@
 package com.pim.stars.planets;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.withSettings;
 
+import org.mockito.Answers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
 
 import com.pim.stars.dataextension.api.DataExtender;
 import com.pim.stars.id.api.IdCreator;
 import com.pim.stars.planets.api.PlanetConfiguration;
 import com.pim.stars.race.api.extensions.RaceId;
-import com.pim.stars.turn.api.policies.GameEntityTransformer;
-import com.pim.stars.turn.api.policies.TurnEntityCreator;
 import com.pim.stars.turn.api.policies.builder.GameToTurnTransformerBuilder;
 
 @Configuration
@@ -28,42 +27,7 @@ public class PlanetTestConfiguration implements PlanetConfiguration.Required {
 	@Bean
 	@Override
 	public GameToTurnTransformerBuilder gameToTurnTransformerBuilder() {
-		return mock(GameToTurnTransformerBuilder.class);
-	}
-
-	@Bean
-	@Primary
-	public TurnEntityCreator<?> planetTurnEntityCreator() {
-		// marked as @Primary and mocked, so that the builder passed into the real method in the real configuration does not have to be mocked.
-		return mock(TurnEntityCreator.class);
-	}
-
-	@Bean
-	@Primary
-	public GameEntityTransformer<?, ?> planetCollectionGameEntityTransformer() {
-		// marked as @Primary and mocked, so that the builder passed into the real method in the real configuration does not have to be mocked.
-		return mock(GameEntityTransformer.class);
-	}
-
-	@Bean
-	@Primary
-	public GameEntityTransformer<?, ?> planetNameEntityTransformer() {
-		// marked as @Primary and mocked, so that the builder passed into the real method in the real configuration does not have to be mocked.
-		return mock(GameEntityTransformer.class);
-	}
-
-	@Bean
-	@Primary
-	public GameEntityTransformer<?, ?> planetOwnerEntityTransformer(final GameToTurnTransformerBuilder builder) {
-		// marked as @Primary and mocked, so that the builder passed into the real method in the real configuration does not have to be mocked.
-		return mock(GameEntityTransformer.class);
-	}
-
-	@Bean
-	@Primary
-	public GameEntityTransformer<?, ?> planetCargoEntityTransformer(final GameToTurnTransformerBuilder builder) {
-		// marked as @Primary and mocked, so that the builder passed into the real method in the real configuration does not have to be mocked.
-		return mock(GameEntityTransformer.class);
+		return mock(GameToTurnTransformerBuilder.class, withSettings().defaultAnswer(Answers.RETURNS_DEEP_STUBS));
 	}
 
 	@Bean
