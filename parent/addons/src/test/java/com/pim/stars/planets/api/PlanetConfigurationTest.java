d009819c5f17e6bc82a17895e9609d5e55ccf6fc
Darkwyng <51367023+Darkwyng@users.noreply.github.com>
2019-11-04 15:10:55 +0100

    extend test

--- parent/addons/src/test/java/com/pim/stars/planets/api/PlanetConfigurationTest.java
+++ parent/addons/src/test/java/com/pim/stars/planets/api/PlanetConfigurationTest.java
@@ -2,6 +2,7 @@ package com.pim.stars.planets.api;
 
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.jupiter.api.Test;
@@ -10,8 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.pim.stars.cargo.api.extensions.CargoDataExtensionPolicy;
+import com.pim.stars.dataextension.api.Entity;
 import com.pim.stars.planets.PlanetTestConfiguration;
 import com.pim.stars.planets.api.extensions.GamePlanetCollection;
+import com.pim.stars.planets.api.extensions.PlanetCargo;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = PlanetTestConfiguration.class)
@@ -19,9 +23,23 @@ public class PlanetConfigurationTest {
 
 	@Autowired
 	private GamePlanetCollection gamePlanetCollection;
+	@Autowired
+	private PlanetCargo planetCargo;
+	@Autowired
+	private CargoDataExtensionPolicy<?> cargoDataExtensionPolicyForGeneric;
+	@Autowired
+	private CargoDataExtensionPolicy<Planet> cargoDataExtensionPolicyForPlanet;
+
+	/** This is how CargoProcessorImp will autowire the policy, so this must work. */
+	@Autowired
+	private CargoDataExtensionPolicy<Entity<?>> cargoDataExtensionPolicyForEntity;
 
 	@Test
 	public void testThatApplicationContextStarts() {
 		assertThat(gamePlanetCollection, not(nullValue()));
+		assertThat(planetCargo, not(nullValue()));
+		assertThat(planetCargo, sameInstance(cargoDataExtensionPolicyForGeneric));
+		assertThat(planetCargo, sameInstance(cargoDataExtensionPolicyForPlanet));
+		assertThat(planetCargo, sameInstance(cargoDataExtensionPolicyForEntity));
 	}
 }
